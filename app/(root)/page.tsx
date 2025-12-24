f228c960ae0d27b46a97823c3840f3bcce9c151b
Muhammad Tayyab <iamtayyab007@Muhammads-MacBook-Pro.local>
2025-12-24 23:01:21 +0500

    implemented the tradingview widgets in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {
+  MARKET_DATA_WIDGET_CONFIG,
+  TOP_STORIES_WIDGET_CONFIG,
+} from "@/lib/contants";
 
 const Home = () => {
-  return <div>Home</div>;
+  const script = "https://s3.tradingview.com/external-embedding/embed-widget-";
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl={`${script}market-overview.js`}
+            config={MARKET_DATA_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+        <div className="md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl={`${script}stock-heatmap.js`}
+            config={MARKET_DATA_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
+
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            // title="Top Stories"
+            scriptUrl={`${script}timeline.js`}
+            config={TOP_STORIES_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            // title="Market Quotes"
+            scriptUrl={`${script}market-quotes.js`}
+            config={MARKET_DATA_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
